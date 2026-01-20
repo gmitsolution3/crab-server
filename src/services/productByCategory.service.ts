1661be2de226b66ee647ff3a7871d891cb7eb6f9
gmitsolution3 <gmitsolution3@gmail.com>
2026-01-20 16:23:28 +0600

    change the category query

--- src/services/productByCategory.service.ts
+++ src/services/productByCategory.service.ts
@@ -1,7 +1,7 @@
 import { productCollection } from "./product.service";
 
 export async function FindProductByCategory(categoryId: string) {
-  const query = { categoryId: categoryId };
+  const query = { categoryId: categoryId, isDraft: false, isDelete: false };
 
   const result = await productCollection
     .find(query)
