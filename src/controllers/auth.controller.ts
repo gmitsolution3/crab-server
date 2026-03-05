ce7586bb7595a7ca039c3e79f3705b64ad09eb66
gmitsolution3 <gmitsolution3@gmail.com>
2026-03-05 13:54:57 +0600

    trying fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -69,7 +69,7 @@ export const logInController = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "lax",
       path: "/",
       maxAge: 3 * 24 * 60 * 60 * 1000,
     });
